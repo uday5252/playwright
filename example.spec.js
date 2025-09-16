1b74df4ffb7de2485574ace60b50f670d879f7f2
uday5252 <73519118+uday5252@users.noreply.github.com>
2025-09-16 10:01:15 +0530

    Add files via upload

--- example.spec.js
+++ example.spec.js
@@ -10,8 +10,22 @@ test("verify login with valid data", async function({page})
 {
     await page.goto("https://www.facebook.com")
 
-    await page.locator("#email").fill("Raju@gmail.com")
-    await page.locator("#pass").fill("Rani@1234")
+    await page.locator("input[data-testid*='roy']").nth(0).fill("Welcome@gmail.com")
+
+    // await page.locator("input[data-testid^='roy']").nth(0).fill("Welcome@gmail.com")
+
+    // await page.locator("input[data-testid='royal-email']").fill("Welcome@gmail.com")
+
+    // await page.locator("[data-testid='royal-email']").fill("Welcome@gmail.com")
+
+    // await page.locator(".inputtext").nth(0).fill("Welcome@gmail.com")
+    // await page.locator(".inputtext").nth(1).fill("Welcome")
+    // await page.locator(".inputtext").first().fill("Raju@gmail.com")
+
+    await page.waitForTimeout(3000)
+
+    // await page.locator("#email").fill("Raju@gmail.com")
+    // await page.locator("#pass").fill("Rani@1234")
 
     // await page.waitForTimeout(5000)
 })
